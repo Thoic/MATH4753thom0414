a7f0a7ef10e162c3fa22a6e2dd279f5d54c68ca6
Nick Thompson <nicholas.j.thompson-1@ou.edu>
2021-03-22 15:29:03 -0500

    added documentation to the fire data set

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' FIREDAM data set.
+#'
+#' A dataset containing the values for firing distance vs. damage
+#'
+#' @usage fire
+#'
+#' @format A data frame with 15 rows and 2 variables:
+#' \describe{
+#'   \item{DISTANCE}{distance away from target}
+#'   \item{DAMAGE}{damage the target took}
+#' }
+#'
+#' @details This is a standard data set from the course MATH4753 taken from the data sets provided by the textbook.
+#'
+#' @source \url{https://www.routledge.com/Statistics-for-Engineering-and-the-Sciences-Sixth-Edition/Mendenhall-Sincich/p/book/9781498728850}
+"fire"
